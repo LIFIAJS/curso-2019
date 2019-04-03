7117df89c8632bb3e820a1505087bf95cb22c85b
lucasdc6 <lucasdicunzolo@gmail.com>
2019-04-03 13:05:25 -0300

    Cambios de instrucciones

--- src/examples/frontend/react/src/index.js
+++ src/examples/frontend/react/src/index.js
@@ -2,5 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<h1>hello</h1>, rootElement);
+ReactDOM.render(<h2>Para probar, importe y reemplaze el h2 por la componente</h2>, rootElement);
 
