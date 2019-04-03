4648c496d69db2dff183651e85bc5e0104ab1f6c
lucasdc6 <lucasdicunzolo@gmail.com>
2019-04-03 16:05:41 -0300

    Cambios en ejemplos

--- src/examples/frontend/react/src/index.js
+++ src/examples/frontend/react/src/index.js
@@ -1,1 +1,21 @@
-import './stateful-component'
+import React, { Fragment } from 'react'
+import ReactDOM from 'react-dom'
+
+const Help = () => (
+  <Fragment>
+    <h2>
+      Sandbox para ejemplos
+    </h2>
+    <ul>
+      <li>
+        Importe alguna de las componentes de src
+      </li>
+      <li>
+        Cambie la componente Help por la componente importada
+      </li>
+    </ul>
+  </Fragment>
+)
+
+const rootElement = document.getElementById('root')
+ReactDOM.render(<Help />, rootElement)
