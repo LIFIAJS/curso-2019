4f07ff8c1033a4af98c4ab96a0f859e23674d165
lucasdc6 <lucasdicunzolo@gmail.com>
2019-04-03 13:08:07 -0300

    Cambios en helpo

--- src/examples/frontend/react/src/index.js
+++ src/examples/frontend/react/src/index.js
@@ -1,6 +1,22 @@
-import React from "react";
+import React, { Fragment } from "react";
 import ReactDOM from "react-dom";
 
+const Help = () => (
+  <Fragment>
+    <h2>
+      Sandbox para ejemplos
+    </h2>
+    <ul>
+      <li>
+        Importe alguna de las compoentes de src
+      </li>
+      <li>
+        Cambie la componente Help por la compoente importada
+      </li>
+    </ul>
+  </Fragment>
+)
+
 const rootElement = document.getElementById("root");
-ReactDOM.render(<h2>Para probar, importe y reemplaze el h2 por la componente</h2>, rootElement);
+ReactDOM.render(<Help />, rootElement);
 
